d76df07def49053c92d5fe81e1d5ce9508bc2b4f
corinnebosley <corinne.e.bosley@gmail.com>
2018-11-24 13:49:34 +0000

    updates to test and start of algorithm

--- lib/best_route/find.py
+++ lib/best_route/find.py
@@ -12,17 +12,46 @@ Python module for finding the shortest route between two given locations
 import argparse
 import collections
 
+import numpy as np
+
+
 def read_network(network):
+    # all_legs is a list of named tuples (legs)
     all_legs = []
+    # all_nodes is a list of every single start and end node
+    all_nodes = []
+    # leg is a named tuple with all info for each leg
     leg = collections.namedtuple('leg', 'startNode endNode distance')
     for line in open(network, 'r'):
-        leg.startnode = line.split(' ')[0]
-        leg.endnode = line.split(' ')[1]
-        leg.distance = line.split(' ')[2]
-        all_legs.append(leg)
+        start = line.split(' ')[0]
+        all_nodes.append(start)
+
+        end = line.split(' ')[1]
+        all_nodes.append(end)
+
+        dist = int(line.split(' ')[2])
+        newleg = leg(start, end, dist)
+
+        all_legs.append(newleg)
+
+    return all_legs, all_nodes
 
 
+def find_closest_neighbour(current_node, all_legs):
+    shortest_dist = np.inf
+    print('current_node: {}'.format(current_node))
+    # print('all_legs: {}'.format(all_legs))
 
+    relevant_legs = []
+    for leg in all_legs:
+        print('this leg: {}, {}, {}'.format(leg.startNode, leg.endNode, leg.distance))
+        # print('leg start node: {}'.format(leg.startNode))
+        if leg.startNode == current_node:
+            relevant_legs.append(leg)
+
+    # relevant_legs = [leg for leg in all_legs if leg.startNode==current_node]
+
+    print(relevant_legs)
 
 
 def main():
@@ -37,8 +66,30 @@ def main():
 
     args = parser.parse_args()
 
-    route_nodes = find_best_route(parser.filename, parser.origin, parser.destination)
-    print(route_nodes)
+    # Create collections of nodes and legs and stuff
+    all_legs, all_nodes = read_network(args.filename)
+    if args.origin not in all_nodes:
+        msg = 'Your start node is not in your requested network.  ' \
+              'Please try a different node or network.'
+        raise ValueError(msg)
+    elif args.destination not in all_nodes:
+        msg = 'Your end node is not in your requested network.  ' \
+              'Please try a different node or network.'
+        raise ValueError(msg)
+
+    unvisited_nodes = set(all_nodes)
+    visited_nodes = set()
+    dist_to_finish = {node: 'inf' for node in unvisited_nodes}
+    current = args.origin
+
+    # Set distance from startpoint as zero
+    dist_to_finish[current] = 0
+
+    find_closest_neighbour(current, all_legs)
+
+    # route_nodes = find_best_route(parser.filename, parser.origin, parser.destination)
+    # print(route_nodes)
+
     # else:
     #     print("I'm afraid there has been a problem, please "
     #           "use 'python env_browser.py --help' to check the arguments you "
--- lib/best_route/tests/test_exmouth-links.py
+++ lib/best_route/tests/test_exmouth-links.py
@@ -8,26 +8,40 @@ import pytest
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-TESTDATA = os.path.abspath(os.path.dirname(os.path.join(HERE,
-                                                        'testdata')))
+TESTDATA = os.path.abspath(os.path.join(HERE, 'testdata'))
+print(TESTDATA)
 
 class TestBestRouteExmouth():
     def setup_class(self):
-        fname = os.path.join(TESTDATA, 'exmouth-links.dat')
-        origin = TBC  # TODO Complete this
-        destination = TBC  # TODO Complete this
-        self.get_best_route = 'best_route {} {} {}'.format(fname,
-                                                           origin,
-                                                           destination)
+        self.fpath = os.path.join(TESTDATA, 'exmouth-links.dat')
+        # origin = None
+        # destination = None
+        # self.get_best_route = 'best_route {} {} {}'.format(fname,
+        #                                                    origin,
+        #                                                    destination)
 
 
-    def test_nodaA_nodeB(self):
-        self.best_route = subprocess.run([self.get_best_route])
+    def test_neighbours(self):
+        fname = self.fname
+        origin = 'J1001'
+        destination = 'J1002'
+        get_best_route = 'best_route {} {} {}'.format(fname,
+                                                      origin,
+                                                      destination)
+        self.best_route = subprocess.run(get_best_route)
         assert subprocess.CompletedProcess.returncode == 0
-        self.output = subprocess.check_output(get_envs,
+        self.output = subprocess.check_output(get_best_route,
                                               stderr=subprocess.STDOUT,
                                               shell=True)
         # TODO Run some checks on the output
+
+    def test_bad_start_node(self):
+        # TODO: This.
+        pass
+
+    def test_bad_end_node(self):
+        # TODO: This.
+        pass
 
 
 if __name__ == '__main__':
