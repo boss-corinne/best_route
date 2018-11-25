55bc4173b272f2faebe184fcc620a647826c7126
corinnebosley <corinne.e.bosley@gmail.com>
2018-11-25 10:47:37 +0000

    tidied up some todos

--- lib/best_route/find.py
+++ lib/best_route/find.py
@@ -2,12 +2,7 @@
 Python module for finding the shortest route between two given locations
 
 """
-# TODO: Find algorithm for finding ther shortest distance between two points
-# TODO: Record source of algorithm in README
-# TODO: Get algorithm working locally according to test parameters (implement it)
 # TODO: Write some tests (from test cases supplied)
-# TODO: Update README instructions accordingly
-# TODO: Work out how to submit this entire package to github, and which repo to sumbit it to.
 
 import argparse
 import collections
